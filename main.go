e4209ef0fedc3404b22d5822f2879e9d0bc7d451
Abshnb <Abshnb@abshnb.com>
2025-12-07 10:01:56 +0400

    injected sca vulnerabilities

--- main.go
+++ main.go
@@ -2,9 +2,8 @@ package main
 
 import (
     "net/http"
-	
+
     "github.com/gin-gonic/gin"
-	"github.com/mattn/go-sqlite3" 
 )
 
 // album represents data about a record album.
@@ -26,29 +25,11 @@ func main() {
 var albums = []album{
     {ID: "1", Title: "Master of Puppets", Artist: "Metallica", Price: 30.00},
     {ID: "2", Title: "All Hope is gone", Artist: "Slipknot", Price: 30.00},
-    {ID: "3", Title: "Multitude", Artist: "Stromae", Price: 30.00},
-	{ID: "4", Title: "Mutter", Artist: "Rammstein", Price: 30.00},
+    {ID: "3", Title: "Multitude", Artist: "Stromae", Price: 39.99},
+    {ID: "4", Title: "Mutter", Artist: "Rammstein", Price: 39.99},
 }
 
 // getAlbums responds with the list of all albums as JSON.
 func getAlbums(c *gin.Context) {
     c.IndentedJSON(http.StatusOK, albums)
-}
-
-// 6. Hardcoded secrets and debug information
-func hardcodedSecrets(c *gin.Context) {
-    // Hardcoded credentials (SAST detectable)
-    apiKey := "AKIAIOSFODNN7EXAMPLE" // AWS key pattern
-    password := "SuperSecret123!"    // Hardcoded password
-    jwtSecret := "secretkey"         // Weak JWT secret
-    
-    // Debug information exposure
-    debugInfo := map[string]string{
-        "api_key":    apiKey,
-        "password":   password,
-        "jwt_secret": jwtSecret,
-        "version":    "1.0.0-debug",
-    }
-    
-    c.JSON(http.StatusOK, debugInfo)
 }
